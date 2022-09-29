24d69409594ec9744eee605288da97edbe1d202a
adsoftsito <adsoft@live.com.mx>
2022-09-29 15:54:08 -0500

    Greet Closes #8 (#9)
    * update README #1
    
    * create greet, add test #8
    
    * add to
    
    * add greet #8

---
+++ src/app/02-strings-arrays/greet.spec.ts
@@ -0,0 +1,9 @@
+import { greet } from './greet';
+
+describe('greet test suite', () => {
+      
+  it('Should include the name in the message', () => {
+    expect(greet('adsoft')).toBe('Welcome adsoft');  
+  })
+
+})
